c1e782fea541688b1c1d02372bc830f8332a3dca
schvabodka-man <scvhapps@gmail.com>
2017-04-18 18:17:47 +0300

    basic implementation of listeners

--- app/src/main/java/apps/scvh/com/farm/ui/activities/CVLayout.java
+++ app/src/main/java/apps/scvh/com/farm/ui/activities/CVLayout.java
@@ -1,11 +1,18 @@
 package apps.scvh.com.farm.ui.activities;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 import apps.scvh.com.farm.R;
 import butterknife.BindView;
@@ -35,28 +42,59 @@ public class CVLayout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cvlayout);
         ButterKnife.bind(this);
-        initDrags();
+        //for fancy init
+        TextView[] arrayOfViews = {name, links, experience, education, projects, primary,
+                secondary, other};
+        ArrayList<TextView> views = new ArrayList<>(Arrays.asList(arrayOfViews));
+        initDrags(views);
     }
 
-    private final class DraggableViews implements View.OnTouchListener {
+
+    private void initDrags(ArrayList<TextView> views) {
+        Iterator<TextView> listIterator = views.iterator();
+        TextView view;
+        while (listIterator.hasNext()) {
+            view = listIterator.next();
+            view.setOnTouchListener(new TouchListener());
+            view.setOnDragListener(new DragListner());
+        }
+    }
+
+    private final class DragListner implements View.OnDragListener {
 
         @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            float x = event.getRawX();
-            float y = event.getRawY();
-            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) v.getLayoutParams();
+        public boolean onDrag(View v, DragEvent event) {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DROP:
+                    View view = (View) event.getLocalState();
+                    ViewGroup owner = (ViewGroup) view.getParent();
+                    owner.removeView(view);
+                    LinearLayout container = (LinearLayout) v;
+                    container.addView(view);
+                    view.setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    break;
+            }
             return true;
         }
     }
 
-    private void initDrags() {
-        name.setOnTouchListener(new DraggableViews());
-        links.setOnTouchListener(new DraggableViews());
-        experience.setOnTouchListener(new DraggableViews());
-        education.setOnTouchListener(new DraggableViews());
-        projects.setOnTouchListener(new DraggableViews());
-        primary.setOnTouchListener(new DraggableViews());
-        secondary.setOnTouchListener(new DraggableViews());
-        other.setOnTouchListener(new DraggableViews());
+    private final class TouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+                        v);
+                v.startDrag(data, shadowBuilder, v, 0);
+                v.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
+
 }
