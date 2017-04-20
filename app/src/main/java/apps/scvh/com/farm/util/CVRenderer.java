0fad5029359046944cd6c9a15e83dc2b1a9373a4
schvabodka-man <scvhapps@gmail.com>
2017-04-20 14:24:59 +0300

    beginning implementation of pdf renderer

---
+++ app/src/main/java/apps/scvh/com/farm/util/CVRenderer.java
@@ -0,0 +1,21 @@
+package apps.scvh.com.farm.util;
+
+
+import android.graphics.pdf.PdfDocument;
+
+public class CVRenderer {
+
+    public PdfDocument renderCV(CV cv) {
+        PdfDocument document = new PdfDocument();
+        return null;
+    }
+
+    public void saveDocument(PdfDocument pdfDocument) {
+
+    }
+
+    public void openDocument(PdfDocument pdfDocument) {
+
+    }
+
+}
